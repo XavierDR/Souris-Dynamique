3da24b14144f9f17176c5fb2828f66d0b0d32344
xavierdr <xavier.drivard@gmail.com>
2015-09-28 12:43:52 -0400

    Added a test LED file for serial com

---
+++ Arduino/LEDTest/LEDTest.ino
@@ -0,0 +1,17 @@
+int msg;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);  
+}
+
+void loop() {
+  msg = Serial.read();
+  Serial.println(msg);
+  if (msg == 'a'){
+    digitalWrite(13, HIGH);
+  }
+  else if(msg =='b'){
+    digitalWrite(13, LOW);
+  }
+}
