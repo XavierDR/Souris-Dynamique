bd6b19af69de5dc0ab5896f2fe055e41f467f276
Xavier Ducharme Rivard <xavier.drivard@gmail.com>
2015-11-02 17:13:01 -0500

    Added addMouse btn and function

--- Python/MainGui.py
+++ Python/MainGui.py
@@ -14,7 +14,7 @@ import Arduino
 class MainGui(QWidget):
     def __init__(self):
         super(MainGui, self).__init__()
-        #self.ard = Arduino.Arduino()
+        self.ard = Arduino.Arduino()
         self.createLayout()
         
     def createLayout(self):
@@ -22,18 +22,28 @@ class MainGui(QWidget):
         # Window setting
         self.mainLayout = QGridLayout()
         self.setLayout(self.mainLayout)
+        
         # Arduino LED button setting
-        self.ledBtn = QPushButton("Activer/Desactivé LED")
+        self.ledBtn = QPushButton("Changer la vitesse")
         self.ledBtn.move(50,50)
-        self.ledBtn.clicked.connect(self.activateLED)
+        self.ledBtn.clicked.connect(self.changeSpeed)
         self.mainLayout.addWidget(self.ledBtn)
+        
+        # Arduino add mouse
+        self.addMouseBtn = QPushButton('Ajouter souris')
+        self.addMouseBtn.move(0, 0)
+        self.addMouseBtn.clicked.connect(self.addMouse)
+        self.mainLayout.addWidget(self.addMouseBtn)
         
         
         self.setWindowTitle('Arduino test')
         self.setGeometry(200, 200, 400, 200)
         self.show()
         
-    def activateLED(self):
+    def addMouse(self):
+        self.ard.addMouse()
+        
+    def changeSpeed(self):
         self.ard.serialCom()
     
 
